c39416033e20b483eb1eb5c9147661c8133b23f8
Marvin Warren <talutha@gmail.com>
2015-08-24 17:54:37 -0500

    Add connect, create, and grab methods for video_share tables

--- lib/talbot/database.rb
+++ lib/talbot/database.rb
@@ -1,5 +1,39 @@
 require 'sequel'
+require_relative 'config'
 
 class DatabaseCalls
+
+  def initialize
+    @config = BotConfig.new
+    connect_to_database
+    create_tables
+    grab_tables
+  end
+
+private
+
+  def connect_to_database
+    user = @config.dbuser
+    pass = @config.dbpassword
+    host = @config.dbhost
+    dbname = @config.dbname
+    @DB = Sequel.connect("postgres://#{user}:#{pass}@#{host}/#{dbname}")
+  end
+
+  # Check the database and make sure the tables required exists
+  def create_tables
+    @DB.run(" CREATE TABLE IF NOT EXISTS discord_video_share(
+                            id int PRIMARY KEY UNIQUE NOT NULL,
+                            url text NOT NULL,
+                            username text NOT NULL,
+                            userid int NOT NULL,
+                            upvotes int NOT NULL,
+                            downvotes int NOT NULL,
+                            timestamp timestamp NOT NULL ); ")
+  end
+
+  def grab_tables
+    @video_share = @DB[:discord_video_share]
+  end
 
 end
